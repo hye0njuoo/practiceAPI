7c832770f145c0227c6372ca44290669f06e3fe3
hyeonju <shj040201@naver.com>
2023-06-24 16:38:15 +0900

    api 호출해보기
    jsonplaceholder사이트에서 제공하는 api를 통해 호출을 xcode에서 구현해봄.

---
+++ PracitceApi/PracitceApi/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  PracitceApi
+//
+//  Created by 성현주 on 2023/06/19.
+//
+
+import UIKit
+import Alamofire
+
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //getTest()
+    }
+    func getTest() {
+        let url = "https://jsonplaceholder.typicode.com/todos/1"
+        AF.request(url, method: .get ,parameters: nil ,encoding: URLEncoding.default,headers: ["Content-Type":"application/json","Accept":"application/json"] )
+            .validate(statusCode: 200..<300)
+            .responseJSON{(json) in
+                print (json)
+
+            }
+
+    }
+    
+        
+    }
+    
+
